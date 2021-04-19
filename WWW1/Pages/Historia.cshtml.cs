f6b16fe5239c6f9cf313d91cff3f32c59778241a
betterrahul5555 <63734203+betterrahul5555@users.noreply.github.com>
2021-04-19 16:16:35 +0200

    Działa wszystko, teraz szczegóły

---
+++ WWW1/Pages/Historia.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WWW1.Date;
+using WWW1.Models;
+using System.Linq;
+
+namespace WWW1.Pages
+{
+    public class HistoriaModel : PageModel
+    {
+
+        FizzContext fizzContext;
+        public HistoriaModel(FizzContext fizz)
+        {
+            fizzContext = fizz;
+        }
+        public IEnumerable<Czas> historia;
+        public void OnGet()
+        {
+             historia=fizzContext.Czas.Take(10).OrderByDescending(o=>o.Dataa);
+
+        }
+    }
+}
--- WWW1/Pages/Index.cshtml.cs
+++ WWW1/Pages/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using WWW1.Date;
 using WWW1.Models;
 
 namespace WWW1.Pages
@@ -21,17 +22,21 @@ namespace WWW1.Pages
 
         [BindProperty(SupportsGet = true)]
         public string Name { get; set; }
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, FizzContext fizz)
         {
             _logger = logger;
+            fizzContext = fizz;
+            
         }
+        private FizzContext fizzContext;
  public IActionResult OnPost()
         {
             if (ModelState.IsValid)
             {
                 HttpContext.Session.SetString("Historia",
                 JsonConvert.SerializeObject(new Czas() { Number = Address.Liczba, St = Address.check() ,Dataa=DateTime.Now }));
-              
+                fizzContext.Add(new Czas() { Number = Address.Liczba, St = Address.check(), Dataa = DateTime.Now });
+                fizzContext.SaveChanges();
             }
             return Page();
         }

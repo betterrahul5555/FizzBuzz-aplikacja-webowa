e8a1117136f347e6e11502bb8a0c472cbf02bc8b
betterrahul5555 <63734203+betterrahul5555@users.noreply.github.com>
2021-05-07 21:58:09 +0200

    sa

---
+++ WWW1/Pages/Historia.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WWW1.Date;
+using WWW1.Models;
+using System.Linq;
+
+namespace WWW1.Pages
+{
+    public class HistoriaModel : PageModel
+    {
+
+        FizzContext fizzContext;
+        [BindProperty]
+        public int ID { get; set; }
+
+        public HistoriaModel(FizzContext fizz)
+        {
+            fizzContext = fizz;
+        }
+        public IEnumerable<Czas> historia;
+        public void OnGet()
+        {
+             historia=fizzContext.Czas.OrderByDescending(o=>o.Dataa).Take(10);
+
+        }
+        public IActionResult OnPost()//po usunieciu  wraca do get (profesionala wersja)
+        {
+            fizzContext.Remove(new Czas() { Id = ID });
+            fizzContext.SaveChanges();
+            
+            return RedirectToPage();
+
+        }
+    }
+}
